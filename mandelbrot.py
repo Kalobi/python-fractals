e2ede71be4ce0abcf78bad70008434cf57fe05ed
Kalobi <46748261+Kalobi@users.noreply.github.com>
2019-01-23 18:21:57 +0100

    generalization and more fractals

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,33 +1,50 @@
 from PIL import Image
 from colour import Color
+import itertools
+from math import pi
 
 
 def normalize_pixel(pixel, size, xrange, yrange):
     return complex(pixel[0] * (xrange[1] - xrange[0]) / size[0] + xrange[0],
-                   pixel[1] * (yrange[1] - yrange[0]) / size[1] + yrange[0])
+                   -(pixel[1] * (yrange[1] - yrange[0]) / size[1] + yrange[0]))
 
 
 def color_map(start, end, depth):
     return Color(start).range_to(Color(end), depth)
 
 
-def mandelbrot(c, iterations, maximum):
-    z = 0 + 0j
+def iterate_bounded(f, c, iterations, bound, initial=0+0j):
+    z = initial
     for i in range(iterations):
-        z = z**2 + c
-        if abs(z) > maximum:
+        z = f(z, c)
+        if abs(z) > bound:
             return i + 1
     return 0
 
 
-im = Image.new("RGB", (3000, 2000))
-depth = 500
-mapping = color_map("blue", "green", depth)
-mapping = [tuple(int(v*256) for v in color.rgb) for color in mapping]
-mapping.insert(0, (0, 0, 0))
-for x in range(im.width):
-    for y in range(im.height):
-        iterations = mandelbrot(normalize_pixel((x, y), im.size, (-2, 1), (-1, 1)), depth, 2)
-        im.putpixel((x, y), mapping[iterations])
+def mandelbrot_map(z, c):
+    return z**2 + c
 
-im.save("result_color.png")
+
+def get_multibrot_map(d):
+    def multibrot_map(z, c):
+        return z**d + c
+    return multibrot_map
+
+
+def burning_ship_map(z, c):
+    return (abs(z.real) + 1j*abs(z.imag))**2 + c
+
+
+def generate_fractal_image(f, height, xrange, yrange, depth):
+    image = Image.new("RGB", (int(height * (xrange[1] - xrange[0]) / (yrange[1] - yrange[0])), height), "white")
+    for pixel in itertools.product(range(image.width), range(image.height)):
+        iterations = iterate_bounded(f, normalize_pixel(pixel, image.size, xrange, yrange), depth, 2)
+        if not iterations:
+            image.putpixel(pixel, (0, 0, 0))
+    return image
+
+
+if __name__ == "__main__":
+    im = generate_fractal_image(burning_ship_map, 600, (-2.5, 1), (-1, 1), 500)
+    im.save("burning_ship.png")
