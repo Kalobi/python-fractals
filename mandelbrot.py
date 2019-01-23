d97041c53cc967f8cd078e068e1d6b74f2043e75
Kalobi <46748261+Kalobi@users.noreply.github.com>
2019-01-23 13:44:04 +0100

    better normalization

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,27 +1,24 @@
 from PIL import Image
 
 
-def pixel_to_complex(pixel):
-    return complex(pixel[0]/250 - 2, pixel[1]/250 - 1)
-
-
 def normalize_pixel(pixel, size, xrange, yrange):
-    return complex()
+    return complex(pixel[0] * (xrange[1] - xrange[0]) / size[0] + xrange[0],
+                   pixel[1] * (yrange[1] - yrange[0]) / size[1] + yrange[0])
 
 
-def mandelbrot(c, iterations, max):
+def mandelbrot(c, iterations, maximum):
     z = 0 + 0j
     for i in range(iterations):
         z = z**2 + c
-        if abs(z) > max:
+        if abs(z) > maximum:
             return i + 1
     return 0
 
 
-im = Image.new("RGB", (1000, 500), "white")
+im = Image.new("RGB", (900, 600), "white")
 for x in range(im.width):
     for y in range(im.height):
-        if not mandelbrot(pixel_to_complex((x, y)), 1000, 2):
+        if not mandelbrot(normalize_pixel((x, y), im.size, (-2, 1), (-1, 1)), 1000, 2):
             im.putpixel((x, y), (0, 0, 0))
 
-im.save("result2.png")
+im.save("result3.png")
